384721d2158b480deeb97258a5ee580f75d19446
gkozhemiakin <kga.2004@mail.ru>
2024-04-25 19:53:11 +0300

    Часть 2. Панель администрирования Django. Регистрация моделей BugReport и FeatureRequest в админ-панели.

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+# Класс администратора для модели BugReport
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'priority', 'created_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'status', 'priority')
+        }),
+    )
+
+    actions = ['change_status']
+
+    def change_status(self, request, queryset):
+        new_status = request.POST.get('new_status', 'Completed')
+        for bug_report in queryset:
+            bug_report.status = new_status
+            bug_report.save()
+
+    change_status.short_description = 'Change status to "Completed"'
+
+
+# Класс администратора для модели FeatureRequest
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'status', 'priority')
+        }),
+    )
