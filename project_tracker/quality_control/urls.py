30d4af627c4618f0edaf9c2616960470fc15dbf5
gkozhemiakin <kga.2004@mail.ru>
2024-04-30 16:32:03 +0300

    Часть 4. VIEW функции и классы

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('bugs/', views.bug_list, name='bug_list'),
     path('features/', views.feature_list, name='feature_list'),
-    path('bugs/<int:bug_id>/', views.bug_detail, name='bug_detail'),
-    path('features/<int:feature_id>/', views.feature_detail, name='feature_detail'),
+    path('bugs/<int:bug_id>/', views.BugReportDetailView.as_view(), name='bug_detail'),
+    path('features/<int:feature_id>/', views.FeatureRequestDetailView.as_view(), name='feature_detail'),
+    path('cbv/', views.IndexView.as_view(), name='index') # Аналогичная главная страница, но с использованием CBV
 ]
--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import BugReport, FeatureRequest
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.views import View
+from django.views.generic import DetailView
+from django.urls import reverse
 
 def index(request):
     html = "<h1>Система контроля качества</h1>"
@@ -10,40 +13,58 @@ def index(request):
     html += "<a href='/quality_control/features/'>Запросы на улучшение</a>"
     return HttpResponse(html)
 
+class IndexView(View):
+    def get(self, request):
+        html = "<h1>Система контроля качества. CBV представление.</h1>"
+        html += "<a href='/quality_control/bugs/'>Список всех багов</a>"
+        html += "<a href='/quality_control/features/'>Запросы на улучшение</a>"
+        return HttpResponse(html)
+
 def bug_list(request):
     html = "<h1>Cписок отчетов об ошибках</h1>"
     bugs = BugReport.objects.all()
     for bug in bugs:
-        html += f"<p>{bug.title} - {bug.project} - {bug.status} - {bug.priority} - {bug.created_at}</p>"
+        html += f"<p>{bug.title} - {bug.status} - </p>"
+        num = bug.id
+        html += f"<a href='/quality_control/bugs/{num}/'>Посмотреть детали</a>"
     return HttpResponse(html)
+
 
 def feature_list(request):
     html = "<h1>Список запросов на улучшение</h1>"
     features = FeatureRequest.objects.all()
     for feature in features:
-        html += f"<p>{feature.title} - {feature.project} - {feature.status} - {feature.priority} - {feature.created_at}</p>"
+        html += f"<p>{feature.title} - {feature.status}</p>"
+        num = feature.id
+        html += f"<a href='/quality_control/features/{num}/'>Посмотреть детали</a>"
     return HttpResponse(html)
 
-def bug_detail(request, bug_id):
-    bug = get_object_or_404(BugReport, id=bug_id)
-    html = f"<h1>Детали бага {bug_id}</h1>"
-    html += f"<p>Название: {bug.title}</p>"
-    html += f"<p>Описание: {bug.description}</p>"
-    html += f"<p>Проект: {bug.project}</p>"
-    html += f"<p>Статус: {bug.status}</p>"
-    html += f"<p>Приоритет: {bug.priority}</p>"
-    html += f"<p>Дата создания: {bug.created_at}</p>"
-    html += f"<p>Дата обновления: {bug.updated_at}</p>"
-    return HttpResponse(html)
+class BugReportDetailView(DetailView):
+    model = BugReport
+    pk_url_kwarg = 'bug_id'
+    def get(self, request, *args, **kwargs):
+        bug = self.get_object()
+        html = f"<h1>Детали бага {bug.id}</h1>"
+        html += f"<p>Название: {bug.title}</p>"
+        html += f"<p>Описание: {bug.description}</p>"
+        html += f"<p>Проект: {bug.project}</p>"
+        html += f"<p>Статус: {bug.status}</p>"
+        html += f"<p>Приоритет: {bug.priority}</p>"
+        html += f"<p>Дата создания: {bug.created_at}</p>"
+        html += f"<p>Дата обновления: {bug.updated_at}</p>"
+        return HttpResponse(html)
 
-def feature_detail(request, feature_id):
-    feature = get_object_or_404(FeatureRequest, id=feature_id)
-    html = f"<h1>Детали улучшения {feature_id}</h1>"
-    html += f"<p>Название: {feature.title}</p>"
-    html += f"<p>Описание: {feature.description}</p>"
-    html += f"<p>Проект: {feature.project}</p>"
-    html += f"<p>Статус: {feature.status}</p>"
-    html += f"<p>Приоритет: {feature.priority}</p>"
-    html += f"<p>Дата создания: {feature.created_at}</p>"
-    html += f"<p>Дата обновления: {feature.updated_at}</p>"
-    return HttpResponse(html)
+class FeatureRequestDetailView(DetailView):
+    model = FeatureRequest
+    pk_url_kwarg = 'feature_id'
+    def get(self, request, *args, **kwargs):
+        feature = self.get_object()
+        html = f"<h1>Детали улучшения {feature.id}</h1>"
+        html += f"<p>Название: {feature.title}</p>"
+        html += f"<p>Описание: {feature.description}</p>"
+        html += f"<p>Проект: {feature.project}</p>"
+        html += f"<p>Статус: {feature.status}</p>"
+        html += f"<p>Приоритет: {feature.priority}</p>"
+        html += f"<p>Дата создания: {feature.created_at}</p>"
+        html += f"<p>Дата обновления: {feature.updated_at}</p>"
+        return HttpResponse(html)
